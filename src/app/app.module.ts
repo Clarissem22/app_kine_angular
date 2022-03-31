afa32fc75e5f7757b59867ee76875a4fc8715873
step31 <cmille749@gmail.com>
2022-03-31 16:19:36 +0200

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProgrammeComponent } from './pages/programme/programme.component';
+import { ExercicesComponent } from './pages/exercices/exercices.component';
+import { ProfilComponent } from './pages/profil/profil.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ParametresComponent } from './pages/parametres/parametres.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DonComponent } from './pages/don/don.component';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProgrammeComponent,
+    ExercicesComponent,
+    ProfilComponent,
+    ContactComponent,
+    ParametresComponent,
+    LoginComponent,
+    DonComponent,
+    NotificationsComponent,
+    InscriptionComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
