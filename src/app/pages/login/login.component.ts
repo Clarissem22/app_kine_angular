90fd0cb0260750ca92ca935dae49c1aaf9ab321f
step31 <cmille749@gmail.com>
2022-04-27 14:33:23 +0200

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,13 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
   logo!:string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.logo = "../assets/img/logologin.png";
-
   }
 
+  onLogin(): void {
+    this.router.navigateByUrl('/home')
+  }
+
+  toInscription():void {
+    this.router.navigateByUrl('/inscription');
+  }
 }
